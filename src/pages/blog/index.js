9ee1efa8d4f7db1febb1bd0d72b5bad9c726e0a7
fgisonni <francesco@mindfulhealth.com>
2019-08-14 16:01:09 -0400

    Remove blog header image

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -3,31 +3,15 @@ import React from 'react'
 import Layout from '../../components/Layout'
 import BlogCard from '../../components/BlogCard'
 
-export default class BlogIndexPage extends React.Component {
+class BlogIndexPage extends React.Component {
+
   render() {
+
     return (
       <Layout>
-        <div
-          className="full-width-image-container margin-top-0"
-          style={{
-            backgroundImage: `url('/img/blog-index.jpg')`,
-          }}
-        >
-          <h1
-            className="has-text-weight-bold is-size-1"
-            style={{
-              boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
-              backgroundColor: '#f40',
-              color: 'white',
-              padding: '1rem',
-            }}
-          >
-            Latest Stories
-          </h1>
-        </div>
         <section className="section">
           <div className="container">
-            <div className="content">
+            <div className="content page-padding">
               <BlogCard />
             </div>
           </div>
@@ -36,3 +20,5 @@ export default class BlogIndexPage extends React.Component {
     )
   }
 }
+
+export default BlogIndexPage
