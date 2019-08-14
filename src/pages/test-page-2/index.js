b03606411e04e3142d24c7c0aa495dcde02fe073
fgisonni <francesco@mindfulhealth.com>
2019-08-14 16:01:44 -0400

    addition of testing page
     anything with 'test name' will be updated before production to it's repspected page name

---
+++ src/pages/test-page-2/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import Layout from '../../components/Layout'
+import BlogCard from '../../components/BlogCard'
+
+export default class BlogIndexTestPage extends React.Component {
+
+  render() {
+
+    return (
+      <Layout>
+        <section className="section">
+          <div className="container">
+            <div className="content page-padding">
+              <BlogCard />
+            </div>
+          </div>
+        </section>
+      </Layout>
+    )
+  }
+}
